7038bb6bf18fbb426f8ba33c75500e6074946ae1
Krzysiek <chrisphilipniznik@gmail.com>
2020-06-30 23:39:37 +0200

    Implement MessageDao interface in dao package

--- src/main/java/dao/MessageDao.java
+++ src/main/java/dao/MessageDao.java
@@ -1,4 +1,13 @@
 package dao;
 
+import model.Message;
+
+import java.util.List;
+
 public interface MessageDao {
+
+    public void create(Message message) throws DaoException;
+
+    public List<Message> getMessages() throws DaoException;
+
 }
