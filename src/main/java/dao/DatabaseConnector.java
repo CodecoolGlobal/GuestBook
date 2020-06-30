19c4c129ed9dcdcdea76a77f01518c08a151ed0c
Krzysiek <chrisphilipniznik@gmail.com>
2020-06-30 23:29:20 +0200

    Implement connection in DataBase Connector

--- src/main/java/dao/DatabaseConnector.java
+++ src/main/java/dao/DatabaseConnector.java
@@ -1,4 +1,23 @@
 package dao;
 
+import java.sql.SQLException;
+
 public class DatabaseConnector {
+    private static BasicDataSource dataSource = new BasicDataSource();
+
+    private DatabaseConnector() {
+
+    }
+
+    static {
+        dataSource.setUrl("jdbc:postgresql://ec2-54-75-246-118.eu-west-1.compute.amazonaws.com/d18e1dete22l3");
+
+        dataSource.setMinIdle(5);
+        dataSource.setMaxIdle(10);
+        dataSource.setMaxOpenPreparedStatements(100);
+    }
+
+    static Connection getConnection() throws SQLException {
+        return  dataSource.getConnection();
+    }
 }
