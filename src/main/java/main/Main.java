83abad5d640568d0a4d3f2bc40b577d6186b9ac1
Krzysiek <chrisphilipniznik@gmail.com>
2020-07-01 00:29:35 +0200

    Implement Main.java

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import com.sun.net.httpserver.HttpServer;
+import controller.Guestbook;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+
+        server.createContext("/guestbook", new Guestbook());
+        server.createContext("/static", new Static());
+        server.setExecutor(null);
+        server.start();
+    }
+}
